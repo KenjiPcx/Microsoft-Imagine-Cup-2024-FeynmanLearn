94d1825c8db9e017ee23ae0d089ae60d36d60e72
Nicolo Micheletti <michelettinik@gmail.com>
2024-02-06 18:18:34 +0100

    Add New Session and Past Session buttons

--- frontend/src/components/NavButton.tsx
+++ frontend/src/components/NavButton.tsx
@@ -1,6 +1,7 @@
 import { ActionIcon, createStyles, keyframes, Image, rem, Tooltip } from "@mantine/core";
-import { IconHome } from "@tabler/icons-react";
+import { IconHome, IconNewSection, IconPhoto } from "@tabler/icons-react";
 import { Link } from "@tanstack/react-router";
+import { useAuth } from "../utils/auth";
 
 const breathe = keyframes`
   0% { transform: translate(0); filter: brightness(1); }
@@ -27,6 +28,7 @@ const useStyles = createStyles((theme) => ({
 
 export const NavButton = () => {
   const { classes } = useStyles();
+  const auth = useAuth();
 
   return (
     <div>
@@ -43,6 +45,42 @@ export const NavButton = () => {
           </ActionIcon>
         </Link>
       </Tooltip>
+      {auth.isAuthenticated && (
+        <Tooltip label="New Session" position="right" withArrow>
+          <Link to="/sessions/new" style={{ textDecoration: 'none' }}>
+            <div style={{ position: 'relative', width: rem(80), height: rem(80), display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              <ActionIcon
+                variant="filled"
+                radius={60}
+                size={rem(60)}
+                style={{ position: 'absolute' }}
+                className={classes.iconButton}
+              >
+                <Image src="/home_button.svg" style={{ position: 'absolute', width: '100%', height: '100%' }} />
+                <IconNewSection size={32} className={classes.icon} />
+              </ActionIcon>
+            </div>
+          </Link>
+        </Tooltip>
+      )}
+      {auth.isAuthenticated && (
+        <Tooltip label="Past Sessions" position="right" withArrow>
+          <Link to="/sessions" style={{ textDecoration: 'none' }}>
+            <div style={{ position: 'relative', width: rem(80), height: rem(60), display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              <ActionIcon
+                variant="filled"
+                radius={60}
+                size={rem(60)}
+                style={{ position: 'absolute' }}
+                className={classes.iconButton}
+              >
+                <Image src="/home_button.svg" style={{ position: 'absolute', width: '100%', height: '100%' }} />
+                <IconPhoto size={32} className={classes.icon} />
+              </ActionIcon>
+            </div>
+          </Link>
+        </Tooltip>
+      )}
     </div>
   );
   
