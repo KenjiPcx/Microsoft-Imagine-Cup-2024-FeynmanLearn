bab82b867bc17af6c66ed7c1cb08e5046a10eeef
Nicolo Micheletti <michelettinik@gmail.com>
2024-02-06 18:05:41 +0100

    Finish Home button

--- frontend/src/components/NavButton.tsx
+++ frontend/src/components/NavButton.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, createStyles, keyframes, Image, rem } from "@mantine/core";
+import { ActionIcon, createStyles, keyframes, Image, rem, Tooltip } from "@mantine/core";
 import { IconHome } from "@tabler/icons-react";
 import { Link } from "@tanstack/react-router";
 
@@ -10,36 +10,18 @@ const breathe = keyframes`
 
 const useStyles = createStyles((theme) => ({
   iconButton: {
-    width: rem(80),
-    height: rem(80),
+    width: rem(60),
+    height: rem(60),
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
     position: 'relative',
     animation: `${breathe} 6s ease-in-out infinite`,
-    '&:hover': {
-      // Define styles for hover state
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[2],
-    },
-    '&:hover $hoverText': {
-      // Show text on hover
-      display: 'block',
-    },
   },
-  iconHome: {
+  icon: {
     position: 'absolute',
-    width: '50%', // Adjust size of IconHome as needed
-    height: '50%', // Adjust size of IconHome as needed
-    zIndex: 2, // Ensure the icon is above the text
-  },
-  hoverText: {
-    display: 'none', // Hide text by default
-    position: 'absolute',
-    bottom: '-20px', // Adjust position as needed
-    whiteSpace: 'nowrap',
-    color: theme.colors.gray[9], // Adjust text color as needed
-    fontSize: theme.fontSizes.sm,
-    zIndex: 1, // Ensure the text does not overlap the icon in an unwanted way
+    width: '50%',
+    height: '50%',
   },
 }));
 
@@ -47,19 +29,23 @@ export const NavButton = () => {
   const { classes } = useStyles();
 
   return (
-    <Link to="/" style={{ textDecoration: 'none', position: 'relative' }}> {/* Use Link component for navigation */}
-      <ActionIcon
-        variant="filled"
-        radius={100}
-        size={rem(80)}
-        className={classes.iconButton}
-      >
-        <Image src="/home_button.svg" style={{ position: 'absolute', width: '100%', height: '100%' }} />
-        <IconHome size={32} className={classes.iconHome} />
-        <span className={classes.hoverText}>Home</span> {/* Text to display on hover */}
-      </ActionIcon>
-    </Link>
+    <div>
+      <Tooltip label="Home" position="right" withArrow>
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <ActionIcon
+            variant="filled"
+            radius={100}
+            size={rem(80)}
+            className={classes.iconButton}
+          >
+            <Image src="/home_button.svg" style={{ position: 'absolute', width: '100%', height: '100%' }} />
+            <IconHome size={32} className={classes.icon} />
+          </ActionIcon>
+        </Link>
+      </Tooltip>
+    </div>
   );
+  
 };
 
 export default NavButton;
