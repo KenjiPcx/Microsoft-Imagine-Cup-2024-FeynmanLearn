1126cbd54511875bcf5167157e6baf7d1c3dffd9
Nicolo Micheletti <michelettinik@gmail.com>
2024-02-08 11:28:07 +0100

    Update Settings Modal

--- frontend/src/components/SettingsModal.tsx
+++ frontend/src/components/SettingsModal.tsx
@@ -1,13 +1,5 @@
-import {
-  Box,
-  Button,
-  Modal,
-  Stack,
-  Text,
-  createStyles,
-} from "@mantine/core";
+import { Box, Button, Modal, Stack, Text, createStyles, CloseButton } from "@mantine/core";
 import { useAuth } from "../utils/auth";
-
 
 interface SettingsModalProps {
   opened: boolean;
@@ -15,13 +7,63 @@ interface SettingsModalProps {
 }
 
 const useStyles = createStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
   header: {
-    backgroundColor: theme.colors.cardFill,
+    backgroundColor: theme.colors.dark[8],
+    padding: theme.spacing.lg,
+    borderTopLeftRadius: theme.radius.md,
+    borderTopRightRadius: theme.radius.md,
+    color: theme.colors.gray[2],
+    position: "relative",
   },
 
   content: {
-    backgroundColor: theme.colors.cardFill,
-    border: `1px solid ${theme.colors.cardStroke}`,
+    backgroundColor: theme.colors.dark[7],
+    padding: theme.spacing.xl,
+    borderBottomLeftRadius: theme.radius.md,
+    borderBottomRightRadius: theme.radius.md,
+    color: theme.colors.gray[0],
+  },
+
+  title: {
+    fontSize: theme.fontSizes.lg,
+  },
+
+  userDetails: {
+    fontSize: theme.fontSizes.md,
+    color: theme.colors.gray[3],
+    marginBottom: theme.spacing.lg,
+    marginTop: theme.spacing.md,
+  },
+
+  logoutButton: {
+    backgroundColor: "linear-gradient(135deg, theme.colors.red[6], theme.colors.cyan[6])",
+    color: theme.white,
+    "&:hover": {
+      backgroundColor: "dark cyan",
+    },
+    marginTop: theme.spacing.md,
+  },
+
+  closeButton: {
+    backgroundColor: "transparent",
+    border: "none",
+    color: theme.colors.gray[2],
+    position: "absolute",
+    top: theme.spacing.xs,
+    right: theme.spacing.xs,
+    opacity: 0.5,
+    "&:hover": {
+      opacity: 1,
+    },
+    "&:focus": {
+      opacity: 1,
+    },
   },
 }));
 
@@ -29,26 +71,34 @@ const SettingsModal = ({ opened, closeSettings }: SettingsModalProps) => {
   const { classes } = useStyles();
   const auth = useAuth();
 
-  console.log("auth data", auth.authData)
   return (
     <Modal
       id="settings-modal"
-      size={"md"}
+      size="sm"
       opened={opened}
       onClose={closeSettings}
-      yOffset={300}
-      title={<Text fw={700}>Settings</Text>}
-      classNames={{ content: classes.content, header: classes.header }}
+      classNames={{
+        header: classes.header,
+        body: classes.content,
+      }}
+      withCloseButton={false}
+      centered
     >
+      <Box className={classes.header}>
+        <Text className={classes.title} weight={700}>
+          Settings
+        </Text>
+        <CloseButton className={classes.closeButton} onClick={closeSettings} />
+      </Box>
       <Stack>
         {auth.isAuthenticated && (
           <Box>
-            <Text>Logged in as {auth.authData.userDetails}</Text>
+            <Text className={classes.userDetails}>Logged in as {auth.authData.userDetails}</Text>
             <Button
-              component={"a"}
+              fullWidth
+              className={classes.logoutButton}
+              component="a"
               href="/.auth/logout"
-              variant="default"
-              mt="auto"
             >
               Log out
             </Button>
